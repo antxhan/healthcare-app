6d44af9d11f6c6e3a83d92ca32701d20fb4ae793
Kevin Garcia Martin <kevingm@live.se>
2025-02-27 10:35:32 +0100

    Revert main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.css';
 import App from './App.jsx';
-import Messages from './pages/messages/Messages.jsx';
-import Layout from './Layout.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/messages" element={<Messages />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
