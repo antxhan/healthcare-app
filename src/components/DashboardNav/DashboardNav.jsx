3acccebe4f7d0f9b35c504292cb2cd21f75dffb6
Kevin Garcia Martin <kevingm@live.se>
2025-03-03 14:07:23 +0100

    Basic navigation styling

--- src/components/DashboardNav/DashboardNav.jsx
+++ src/components/DashboardNav/DashboardNav.jsx
@@ -2,23 +2,36 @@ import './DashboardNav.css';
 import { NavLink } from 'react-router-dom';
 import { dashboardRouter } from '../../routers/DashboardRouter/DashboardRouter';
 import { capitalizeFirstLetter } from '../../utils/utils';
+import userSVG from '../../assets/user.svg';
 
 export default function DashboardNav() {
   const routes = dashboardRouter.routes[0].children;
 
   return (
     <aside className="dashboard-nav">
+      <div className="user">
+        <img src={userSVG} alt="User avatar" />
+        <p className="user__name">John Doe</p>
+      </div>
+      <hr />
       <nav>
         <ul>
-          {routes.map((route) => (
-            <li key={route.path}>
-              <NavLink to={route.path}>
-                {route.path === '/'
-                  ? 'Home'
-                  : capitalizeFirstLetter(route.path.slice(1))}
-              </NavLink>
-            </li>
-          ))}
+          {routes
+            .filter((route) => route.path !== '/settings')
+            .map((route) => (
+              <li key={route.path}>
+                <NavLink to={route.path}>
+                  {route.path === '/'
+                    ? 'Home'
+                    : capitalizeFirstLetter(route.path.slice(1))}
+                </NavLink>
+              </li>
+            ))}
+        </ul>
+        <ul>
+          <li>
+            <NavLink to={'/settings'}>{'Settings'}</NavLink>
+          </li>
         </ul>
       </nav>
     </aside>
