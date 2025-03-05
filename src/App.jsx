73b03ce325c196dc5c08e65afe0ced59cd73cf1b
Kevin Garcia Martin <kevingm@live.se>
2025-03-05 11:24:39 +0100

    Refactor App to include header in RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { dashboardRouter } from './routers/DashboardRouter/DashboardRouter';
-import Header from './components/Header/Header';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <RouterProvider router={dashboardRouter} />
-    </>
-  );
+  return <RouterProvider router={dashboardRouter} />;
 }
 
 export default App;
