3e8ad425bb9e773d9aa6ed1979d650a958d8c57c
Kevin Garcia Martin <kevingm@live.se>
2025-03-05 11:25:25 +0100

    Add Header

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,15 +1,19 @@
 import './DashboardLayout.css';
 import { Outlet } from 'react-router-dom';
+import Header from '../components/Header/Header';
 import DashboardNav from '../components/DashboardNav/DashboardNav';
 import DashboardContent from '../components/DashboardContent/DashboardContent';
 
 export default function DashboardLayout() {
   return (
-    <section className="dashboard">
-      <DashboardNav />
-      <DashboardContent>
-        <Outlet />
-      </DashboardContent>
-    </section>
+    <>
+      <Header />
+      <section className="dashboard">
+        <DashboardNav />
+        <DashboardContent>
+          <Outlet />
+        </DashboardContent>
+      </section>
+    </>
   );
 }
