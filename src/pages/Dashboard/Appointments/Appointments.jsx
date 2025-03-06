e6163e4f677bc273a645db2d981b497761e48cdf
Kevin Garcia Martin <kevingm@live.se>
2025-03-06 14:57:36 +0100

    Add Appointments content

--- src/pages/Dashboard/Appointments/Appointments.jsx
+++ src/pages/Dashboard/Appointments/Appointments.jsx
@@ -1,5 +1,41 @@
+import { useState } from 'react';
 import PageTitle from '../../../components/PageTitle/PageTitle';
+import Button from '../../../components/Button/Button';
+import CardList from '../../../components/CardList/CardList';
+import Card from '../../../components/Card/Card';
 
 export default function Appointments() {
-  return <PageTitle title={'Appointments'} />;
+  const [cardInformation] = useState([
+    {
+      name: 'Karolinska Institutet',
+      information: [
+        {
+          'Date & Time': 'March 2, 2025 1PM',
+          Doctor: 'Jane Doe',
+          Department: 'X-Ray Department',
+        },
+      ],
+      footerButtons: ['Reschedule'],
+    },
+  ]);
+
+  return (
+    <>
+      <header>
+        <PageTitle title={'Appointments'} />
+        <Button isPrimary={true} text={'Book'} />
+      </header>
+      <CardList>
+        {cardInformation.map((card, index) => (
+          <li key={card.name + index}>
+            <Card
+              title={card.name}
+              information={card.information}
+              footerButtons={card.footerButtons}
+            />
+          </li>
+        ))}
+      </CardList>
+    </>
+  );
 }
