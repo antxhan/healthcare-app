9f1f9cebece9082289fdbd38427de281d9e9cb4d
Kevin Garcia Martin <kevingm@live.se>
2025-03-06 14:14:24 +0100

    Add Medication content

--- src/pages/Dashboard/Medication/Medication.jsx
+++ src/pages/Dashboard/Medication/Medication.jsx
@@ -1,5 +1,37 @@
+import { useState } from 'react';
 import PageTitle from '../../../components/PageTitle/PageTitle';
+import CardList from '../../../components/CardList/CardList';
+import Card from '../../../components/Card/Card';
 
 export default function Medication() {
-  return <PageTitle title={'Medication'} />;
+  const [cardInformation] = useState([
+    {
+      name: 'Loratidin',
+      information: [
+        {
+          Date: 'April 1, 2023',
+          From: 'Jane Doe',
+          Dosage: '2 times daily',
+          'Remaining prescription': 0,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <>
+      <PageTitle title={'Medication'} />
+      <CardList>
+        {cardInformation.map((card, index) => (
+          <li key={card.name + index}>
+            <Card
+              title={card.name}
+              subtitles={card.subtitles}
+              information={card.information}
+            />
+          </li>
+        ))}
+      </CardList>
+    </>
+  );
 }
