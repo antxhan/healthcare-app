05a917537be150761de5a3c5eb3c0a6774082983
Kevin Garcia Martin <kevingm@live.se>
2025-03-06 12:28:00 +0100

    Add Record content

--- src/pages/Dashboard/Record/Record.jsx
+++ src/pages/Dashboard/Record/Record.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import { useState } from 'react';
+import PageTitle from '../../../components/PageTitle/PageTitle';
+import CardList from '../../../components/CardList/CardList';
+import Card from '../../../components/Card/Card';
 
 export default function Record() {
+  const [cardInformation] = useState([
+    {
+      name: 'Bone surgery',
+      information: [
+        {
+          'Date & Time': 'May 1, 2023 2PM',
+          From: 'Jane Doe',
+          Message: 'Fix patients broken bones.',
+        },
+      ],
+    },
+    {
+      name: 'X-ray visit',
+      information: [
+        {
+          'Date & Time': 'April 1, 2023 2PM',
+          From: 'Jane Doe',
+          Message: 'Took an X-ray, patient has broken bones.',
+        },
+      ],
+    },
+    {
+      name: 'Hospital visit',
+      information: [
+        {
+          'Date & Time': 'March 1, 2023 2PM',
+          From: 'Jane Doe',
+          Message: 'Patient noted they had hurt their leg lol.',
+        },
+      ],
+    },
+  ]);
+
   return (
-    <div>Record</div>
-  )
+    <>
+      <PageTitle title={'Home'} />
+      <CardList>
+        {cardInformation.map((card, index) => (
+          <li key={card.name + index}>
+            <Card
+              title={card.name}
+              subtitles={card.subtitles}
+              information={card.information}
+            />
+          </li>
+        ))}
+      </CardList>
+    </>
+  );
 }
