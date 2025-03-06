bf61bd2ec4a947a4fc31332827f96abded08b599
Kevin Garcia Martin <kevingm@live.se>
2025-03-06 14:36:00 +0100

    Add YourHealthCenter content

--- src/pages/Dashboard/YourHealthCenter/YourHealthCenter.jsx
+++ src/pages/Dashboard/YourHealthCenter/YourHealthCenter.jsx
@@ -1,5 +1,36 @@
+import { useState } from 'react';
 import PageTitle from '../../../components/PageTitle/PageTitle';
+import CardList from '../../../components/CardList/CardList';
+import Card from '../../../components/Card/Card';
 
 export default function YourHealthCenter() {
-  return <PageTitle title={'Your Health Center'} />;
+  const [cardInformation] = useState([
+    {
+      name: 'Flemingsbergs Vårdcentral',
+      information: [
+        {
+          Address: 'Röntgenvägen 3, 141 52 Huddinge',
+          Phone: '08-123 425 00',
+        },
+      ],
+      footerButtons: ['Change'],
+    },
+  ]);
+
+  return (
+    <>
+      <PageTitle title={'Your Health Center'} />
+      <CardList>
+        {cardInformation.map((card, index) => (
+          <li key={card.name + index}>
+            <Card
+              title={card.name}
+              information={card.information}
+              footerButtons={card.footerButtons}
+            />
+          </li>
+        ))}
+      </CardList>
+    </>
+  );
 }
